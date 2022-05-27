09d015679e1b72f65e1dd736ee05ae768bf26434
francescvr <101355262+francescvr@users.noreply.github.com>
2022-05-27 15:27:43 +0200

    Update main.cpp

--- Practica_1/src/main.cpp
+++ Practica_1/src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+
+/* delete a task when finish,
+this will never happen because this is infinity loop */
+
+
+void funcion1(void * parameter)
+{
+#define led 16
+pinMode(led, OUTPUT);
+
+for(;;)
+{
+digitalWrite(led, HIGH);
+Serial.println("ON");
+delay(1000);
+digitalWrite(led, LOW);
+Serial.println("OFF");vTaskDelete (NULL);
+delay(1000);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 }
+
+void setup()
+{
+Serial.begin(112500);
+
+xTaskCreate(
+funcion1, /* Task function. */
+"FUNCION 1", /* name of task. */
+10000, /* Stack size of task */
+NULL, /* parameter of the task */
+1, /* priority of the task */
+NULL); /* Task handle to keep track of created task */
+}
+
+
+void loop()
+{}
